5097a8ea22eb14ee73d56c789522eb9dc947329c
gang.dun <dungang@126.com>
2019-05-24 16:28:15 +0800

    拆分资源文件管理独立插件,方便后期有更好的方案替代升级

--- kit/core/CoreAssetManager.php
+++ kit/core/CoreAssetManager.php
@@ -2,7 +2,7 @@
 namespace app\kit\core;
 
 use yii\web\AssetManager;
-use app\kit\models\Asset;
+use app\kit\events\CustomerEvent;
 
 /**
  *
@@ -10,19 +10,26 @@ use app\kit\models\Asset;
  */
 class CoreAssetManager extends AssetManager {
 
+    const EVENT_BEORE_GET_BUNDLE = 'beforeGetBundle';
+
     private static $__assets;
 
     public function getBundle($name, $publish = true) {
-        if (empty(self::$__assets) && (self::$__assets = Asset::getActiveAssets())) {
+        $this->beforeGetBundle();
+        if (self::$__assets) {
+            //加载功能的资源
             $common = isset(self::$__assets['common']) ? self::$__assets['common'] : [];
             $bundles = [];
             $module_id = \Yii::$app->controller->module->id;
+            //如果是后台的请求，则加载后台的公共资源
             if (\Yii::$app->controller instanceof BackendController) {
                 $bundles = isset(self::$__assets['backend']) ? self::$__assets['backend'] : [];
             } else {
+                //否则就是前端请求，则加载前端的公共资源
                 if (!empty(self::$__assets['frontend'])) {
                     $bundles = self::$__assets['frontend'];
                 }
+                //如果前端有针对插件模块有特殊的资源，则加载
                 if (isset(self::$__assets[$module_id])) {
                     $bundles = \array_merge($bundles, self::$__assets[$module_id]);
                 }
@@ -30,6 +37,12 @@ class CoreAssetManager extends AssetManager {
             $this->bundles = array_merge($this->bundles, $common, $bundles);
         }
         return parent::getBundle($name, $publish);
+    }
+
+    public function beforeGetBundle(){
+        $event = new CustomerEvent();
+        $this->trigger(self::EVENT_BEORE_GET_BUNDLE,$event);
+        self::$__assets = $event->payload;
     }
 
 }
