ba2d4ad61a564d366118c706fa1807271303b126
gang.dun <dungang@126.com>
2020-04-24 23:02:00 +0800

    support gradient

--- themes/basic/site/index.php
+++ themes/basic/site/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\addons\flash\helpers\FlashHelpers;
 use app\addons\flash\models\FeFlash;
 use app\kit\widgets\Swiper;
 ?>
@@ -8,6 +9,9 @@ use app\kit\widgets\Swiper;
     'items'=> FeFlash::find()->orderBy('sort desc')->limit(5)->all(),
     'pagination'=>false,
     'slideContentCallback'=>function($item,$index){
+        if($item['bg_color']) {
+            return '<div style="width:100%;height:390px;'.FlashHelpers::gradient_radial().'"></div>';
+        }
         return '<img src="'.$item['pic'].'" width="100%" style="display:block;max-height:390px;"/>';
     }
 ])?>
