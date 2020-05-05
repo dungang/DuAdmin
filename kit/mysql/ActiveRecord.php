523a0e9d0e3c8cf2a2e69707ae70ea7b26492ac5
dungang <dungang@126.com>
2020-05-05 18:07:05 +0800

    add activerecord findone findall support for update lock

--- kit/mysql/ActiveRecord.php
+++ kit/mysql/ActiveRecord.php
@@ -3,9 +3,14 @@
 namespace app\kit\mysql;
 
 use Yii;
-use yii\db\ActiveRecord as BaseActiveRecord;
+use yii\db\ActiveRecord as OriginActiveRecord;
 
-class ActiveRecord extends BaseActiveRecord
+/**
+ * 支持select for update 的activerecord
+ * 
+ * @method \app\kit\mysql\ActiveQuery findByCondition($condition)
+ */
+class ActiveRecord extends OriginActiveRecord
 {
     /**
      * @inheritdoc
@@ -14,5 +19,23 @@ class ActiveRecord extends BaseActiveRecord
     public static function find()
     {
         return Yii::createObject(ActiveQuery::className(), [get_called_class()]);
+    }
+
+    /**
+     * @param $condition mixed
+     * @return static|null ActiveRecord instance matching the condition, or `null` if nothing matches.
+     */
+    public function findOneForUpdate($condition)
+    {
+        return static::findByCondition($condition)->forUpdate()->one();
+    }
+
+    /**
+     * @param $condition mixed
+     * @return static[] an array of ActiveRecord instances, or an empty array if nothing matches.
+     */
+    public static function findAllForUpdate($condition)
+    {
+        return static::findByCondition($condition)->forUpdate()->all();
     }
 }
