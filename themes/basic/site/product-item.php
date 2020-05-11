5999b5581ff71423e28b8dd8ba22fe85c236e0cd
dungang <dungang@126.com>
2020-05-11 10:49:00 +0800

    add nopic.png

--- themes/basic/site/product-item.php
+++ themes/basic/site/product-item.php
@@ -5,7 +5,9 @@ use yii\helpers\Html;
 ?>
 <div class="col-xs-6 col-sm-6 col-md-3">
     <div class="thumbnail">
-        <?= Html::img($model->cover) ?>
+        <div class="img-aspect-11">
+          <?= Html::img(empty($model->cover)?'images/nopic.png':$model->cover) ?>
+        </div>
         <div class="caption">
             <h3><?=Html::a($model->title,['/site/page','slug'=>$model->slug])?></h3>
         </div>
