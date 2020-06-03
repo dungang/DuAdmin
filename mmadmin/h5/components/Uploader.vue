2d6b416f5671dd064a89ae6d907d274c3a0adc90
dungang <dungang@126.com>
2020-06-03 18:45:24 +0800

    fix

--- mmadmin/h5/components/Uploader.vue
+++ mmadmin/h5/components/Uploader.vue
@@ -5,12 +5,25 @@
 import Vue from "vue";
 import { Uploader } from "vant";
 import Moment from "moment";
+import { compress } from "../lib/Utils";
 Vue.use(Uploader);
 export default {
   props: {
     dir: {
       type: String,
       default: "image"
+    },
+    compress: {
+      type: Boolean,
+      default: false
+    },
+    maxCount: {
+      type: [Number, String],
+      default: 1
+    },
+    maxSize: {
+      type: [Number, String],
+      default: 1 * 1024 * 1024
     }
   },
   data() {
@@ -21,35 +34,41 @@ export default {
   methods: {
     afterRead(file) {
       if (MA && MA.uploader) {
-        this.$api.get("/site/token", {}, res => {
-          if (res.status == 200) {
-            let token = res.data;
-            let form = new FormData();
-            let key = this.getObjKey(file.file);
-            form.append(MA.uploader.keyName, key);
-            form.append("file", file.file);
-            form.append(MA.uploader.tokenName, token);
-            file.status = "uploading";
-            this.$api.upload(
-              MA.uploader.resource,
-              form,
-              res => {
-                file.url = MA.uploader.baseUrl + key;
-                file.status = "done";
-              },
-              error => {
-                file.statue = "failed";
-              }
-            );
-          }
+        compress(file.content).then(data => {
+          this.uploaderFile(file,data);
         });
       }
+    },
+    uploaderFile(file,data) {
+      this.$api.get("/site/token", {}, res => {
+        if (res.status == 200) {
+          let token = res.data;
+          let form = new FormData();
+          let key = this.getObjKey(file.file);
+          form.append(MA.uploader.keyName, key);
+          form.append("file", data);
+          form.append(MA.uploader.tokenName, token);
+          file.status = "uploading";
+          this.$api.upload(
+            MA.uploader.resource,
+            form,
+            res => {
+              file.url = MA.uploader.baseUrl + key;
+              file.status = "done";
+            },
+            error => {
+              file.statue = "failed";
+            }
+          );
+        }
+      });
     },
     getObjKey(file) {
       return (
         this.dir +
         "/" +
-        Moment().format("L") + "/" +
+        Moment().format("L") +
+        "/" +
         Moment().valueOf() +
         "." +
         this.getExtension(file.name)
---
+++ mmadmin/h5/lib/Utils.js
@@ -0,0 +1,27 @@
+export const compress = (image) => {
+    return new Promise(function (resolve, reject) {
+        try {
+            let img = new Image(),
+                maxH = 160;
+            img.onload = () => {
+                let cvs = document.createElement('canvas'),
+                    ctx = cvs.getContext('2d');
+                if (img.height > maxH) {
+                    img.width *= maxH / img.height;
+                    img.height = maxH;
+                }
+                cvs.width = img.width;
+                cvs.height = img.height;
+                ctx.clearRect(0, 0, cvs.width, cvs.height);
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+                cvs.toBlob((data)=>{
+                    resolve(data)
+                },'image/jpeg', 0.6)
+            }
+            img.src = image;
+        } catch (e) {
+            console.log(e);
+            reject(new Error(e.getMessage()));
+        }
+    });
+}
