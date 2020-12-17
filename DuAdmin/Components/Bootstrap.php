afbf16a53e6323af1aa021c52f2b1d9156d9b479
dungang <dungang@126.com>
2020-12-17 21:47:23 +0800

    增加readme

--- DuAdmin/Components/Bootstrap.php
+++ DuAdmin/Components/Bootstrap.php
@@ -28,36 +28,6 @@ class Bootstrap implements BootstrapInterface
      */
     public function bootstrap($app)
     {
-//         // 注册项目的别名
-//         // 没有注册 DuAdmin目录，因为有安装scripts,所以使用了composer的autoload
-//         // 一下文字是引用的来源 https://www.yiichina.com/doc/guide/2.0/concept-autoloading
-//         // 你也可以只使用 Composer 的自动加载，而不用 Yii 的自动加载。
-//         // 不过这样做的话，类的加载效率会下降， 且你必须遵循 Composer 所设定的规则，
-//         // 从而让你的类满足可以被自动加载的要求。
-//         Yii::setAlias('@Addons',$app->basePath . '/Addons');
-//         Yii::setAlias('@Backend',$app->basePath . '/Backend');
-//         Yii::setAlias('@Frontend',$app->basePath . '/Frontend');
-//         Yii::setAlias('@Api',$app->basePath . '/Api');
-//         Yii::setAlias('@Console',$app->basePath . '/Console');
-        
-        
-//         // 更换mysql的schema对象，支持for update 排他锁
-//         $app->db->schemaMap['mysql'] = 'DuAdmin\Mysql\Schema';
-//         $app->db->schemaMap['mysqli'] = 'DuAdmin\Mysql\Schema';
-
-//         // 增加自定义的DB查询条件表达式
-//         $app->db->queryBuilder->setExpressionBuilders([
-//             'DuAdmin\Db\DateRangeCondition' => 'DuAdmin\Db\DateRangeConditionBuilder'
-//         ]);
-//         $app->db->queryBuilder->setConditionClasses([
-//             'DATE_RANGE' => 'DuAdmin\Db\DateRangeCondition'
-//         ]);
-//         $app->db->queryBuilder->setExpressionBuilders([
-//             'DuAdmin\Db\FullSearchCondition' => 'DuAdmin\Db\FullSearchConditionBuilder'
-//         ]);
-//         $app->db->queryBuilder->setConditionClasses([
-//             'FULL_SEARCH' => 'DuAdmin\Db\FullSearchCondition'
-//         ]);
         
         // 注册DUAdmin的多语言
         $app->i18n->translations['da'] = [
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Yii Fast Developement Framework
+# DuAdmin 独角派管理后台
+
+DuAdmin（独角派管理后台）yii2管理后台快速开发包
