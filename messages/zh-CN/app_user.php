6dff7ddf28e1b4ac0666ba1e03e35c28e1e03964
gang.dun <dungang@126.com>
2020-12-28 22:52:32 +0800

    更新样式

---
+++ messages/zh-CN/app_user.php
@@ -0,0 +1,20 @@
+<?php 
+return [
+	'User' => '用户',
+	'Users' => '用户',
+	'Username' => '用户名',
+	'Nickname' => '昵称',
+	'Avatar' => '头像',
+	'Auth Key' => '授权KEY',
+	'Password Hash' => '密码hash',
+	'Password Reset Token' => '密码重置token',
+	'Email' => '邮箱',
+	'Mobile' => '手机',
+	'Status' => '状态',
+	'Is Super' => '是否创始用户',
+	'Login At' => '上次登陆时间',
+	'Login Failure' => '登陆失败消息',
+	'Login Ip' => '登录IP',
+	'Created At' => '添加时间',
+	'Updated At' => '更新时间',
+];
