9851793b27c3b970189c85a78b4fd0752dc59f76
gang.dun <dungang@126.com>
2020-12-29 22:55:21 +0800

    remove behavior

---
+++ DuAdmin/Grids/MultilingualAction.php
@@ -0,0 +1,20 @@
+<?php
+namespace DuAdmin\Grids;
+
+use yii\grid\Column;
+
+class MultilingualAction extends Column
+{
+    /**
+     * {@inheritDoc}
+     * @see \yii\grid\Column::renderDataCellContent()
+     */
+    protected function renderDataCellContent($model, $key, $index)
+    {
+        // TODO Auto-generated method stub
+        return parent::renderDataCellContent();
+    }
+
+    
+}
+
