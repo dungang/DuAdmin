690b9e937a3a5f335dc917e17e3aeface1c396c0
dungang <dungang@126.com>
2021-06-23 14:04:34 +0800

    change asset

--- DuAdmin/Assets/DuAdminAsset.php
+++ DuAdmin/Assets/DuAdminAsset.php
@@ -1,31 +1,31 @@
 <?php
+
 namespace DuAdmin\Assets;
 
 use yii\web\AssetBundle;
 
-class DuAdminAsset extends AssetBundle
-{
-    public $baseUrl = '@web';
-    
-    public $css = [
-        'third/font-awesome/css/font-awesome.min.css',
-        'duadmin/dist/css/DUAdmin.css'
-    ];
-    
-    public $js = [
-        'js/jquery.form.min.js',
-        'js/holder.min.js',
-        'duadmin/dist/js/DUAdmin.js',
-        
-    ];
-    
-    public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
-        'yii\validators\ValidationAsset',
-        'yii\widgets\ActiveFormAsset',
-        'DuAdmin\Assets\GoogleFontsSourceSansProAsset',
-        'DuAdmin\Assets\NotifyAsset',
-    ];
+class DuAdminAsset extends AssetBundle {
+
+  public $baseUrl = '@web';
+
+  public $css = [
+      'third/font-awesome/css/font-awesome.min.css',
+      'duadmin/dist/css/DUAdmin.css'
+  ];
+
+  public $js = [
+      'js/jquery.form.min.js',
+      'js/holder.min.js',
+      'duadmin/dist/js/DUAdmin.js'
+  ];
+
+  public $depends = [
+      '\yii\web\YiiAsset',
+      '\yii\bootstrap\BootstrapPluginAsset',
+      '\yii\validators\ValidationAsset',
+      '\yii\widgets\ActiveFormAsset',
+      '\DuAdmin\Assets\GoogleFontsSourceSansProAsset',
+      '\DuAdmin\Assets\NotifyAsset'
+  ];
 }
 
