bf3c4e1515ae9329bf9582f25f0c495ba87063ce
dungang <dungang@126.com>
2021-06-28 12:13:14 +0800

    update clean-assets console

--- Console/CleanAssetsController.php
+++ Console/CleanAssetsController.php
@@ -1,28 +1,34 @@
 <?php
+
 namespace Console;
 
 use yii\console\Controller;
-use yii\helpers\FileHelper;
 use yii\helpers\Console;
+use yii\helpers\FileHelper;
 
 /**
  * 清理assets
+ *
  * @author dunga
  *
  */
-class CleanAssetsController extends Controller
-{
-    /**
-     * 清理assets
-     */
-    public function actionIndex(){
-        $dirs = FileHelper::findDirectories(\Yii::$app->basePath . '/public/assets',['recursive'=>false]);
-        if($dirs) {
-            foreach($dirs as $dir) {
-                FileHelper::removeDirectory($dir);
-            }
-        }
-        Console::output(json_encode($dirs));
+class CleanAssetsController extends Controller {
+
+  /**
+   * 清理assets
+   */
+  public function actionIndex() {
+
+    $dirs = FileHelper::findDirectories( \Yii::$app->basePath . '/Public/assets', [
+        'recursive' => false
+    ] );
+    if ( $dirs ) {
+      foreach ( $dirs as $dir ) {
+        FileHelper::removeDirectory( $dir );
+      }
     }
+    Console::output( json_encode( $dirs ) );
+
+  }
 }
 
