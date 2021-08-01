9397b9f5d42deb59d21fe6a34e89b6425e01231c
dungang <dungang@126.com>
2021-08-01 22:21:47 +0800

    add home banner / search

--- Addons/Cms/PageBlock/Layouts/HomeBanners/views/home-banner-code.php
+++ Addons/Cms/PageBlock/Layouts/HomeBanners/views/home-banner-code.php
@@ -1,9 +1,13 @@
 <div class="home-banner du-live-element img-holder" style="background-image: url('/images/computer.jpg')">
-    <div class="container">
-        <h1>This is the default
-            <br/>
-            template of the website builder</h1>
-        <p>Drag and drop website builder</p>
-        <p>and CMS of new generation</p>
+    <div class="container du-live-element-layout">
+
+        <div class="du-live-element">
+            <h1>This is the default
+                <br/>
+                template of the website builder</h1>
+            <p>Drag and drop website builder</p>
+            <p>and CMS of new generation</p>
+        </div>
+
     </div>
 </div>
