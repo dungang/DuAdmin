5217d86d0dadc6c4f3fe7ea2baa2d0b71458c204
gang.dun <dungang@126.com>
2021-11-14 20:54:32 +0800

    fix batch delete

--- Addons/Cms/Controllers/Backend/FriendLinkController.php
+++ Addons/Cms/Controllers/Backend/FriendLinkController.php
@@ -14,146 +14,147 @@ use yii\widgets\ActiveForm;
  * FriendLink 模型的控制器
  * FriendLinkController 实现了常规的增删查改等行为
  */
-class FriendLinkController extends BackendController {
+class FriendLinkController extends BackendController
+{
 
-  public function actions() {
+    public function actions()
+    {
 
-    return [
-        'sorts' => [
-            'class' => '\DuAdmin\Core\SortableAction',
-            'modelClass' => FriendLink::class
-        ]
-    ];
-
-  }
-
-  /**
-   * 列出所有的 FriendLink 模型.
-   *
-   * @return mixed
-   */
-  public function actionIndex() {
-
-    $models = FriendLink::find()->asArray()->orderBy( 'sort' )->all();
-    return $this->render( 'index', [
-        'models' => $models
-    ] );
-
-  }
-
-  /**
-   * 显示单个的 FriendLink 模型数据.
-   *
-   * @param integer $id
-   * @return mixed
-   * @throws NotFoundHttpException if the model cannot be found
-   */
-  public function actionView( $id ) {
-
-    return $this->render( 'view', [
-        'model' => $this->findModel( $id )
-    ] );
-
-  }
-
-  /**
-   * 创建一个新的 FriendLink 模型.
-   * 如果创建成果,浏览器将会跳转的到该模型的详情视图界面.
-   *
-   * @return mixed
-   */
-  public function actionCreate() {
-
-    $model = new FriendLink();
-    // ajax表单验证
-    if ( AppHelper::isAjaxValidationRequest() && $model->load( Yii::$app->request->post() ) ) {
-      Yii::$app->response->format = Response::FORMAT_JSON;
-      return ActiveForm::validate( $model );
+        return [
+            'sorts' => [
+                'class' => '\DuAdmin\Core\SortableAction',
+                'modelClass' => FriendLink::class
+            ]
+        ];
     }
-    if ( $model->load( Yii::$app->request->post() ) && $model->save() ) {
-      return $this->redirectSuccess( [
-          'view',
-          'id' => $model->id
-      ], "添加成功" );
+
+    /**
+     * 列出所有的 FriendLink 模型.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+
+        $models = FriendLink::find()->asArray()->orderBy('sort')->all();
+        return $this->render('index', [
+            'models' => $models
+        ]);
     }
-    return $this->render( 'create', [
-        'model' => $model
-    ] );
 
-  }
+    /**
+     * 显示单个的 FriendLink 模型数据.
+     *
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
 
-  /**
-   * 更新一条已经存在的 FriendLink 模型.
-   * 如果更新成果,浏览器将会跳转的到该模型的详情视图界面.
-   *
-   * @param integer $id
-   * @return mixed
-   * @throws NotFoundHttpException 如果模型没查询到
-   */
-  public function actionUpdate( $id ) {
-
-    $model = $this->findModel( $id );
-    // ajax表单验证
-    if ( AppHelper::isAjaxValidationRequest() && $model->load( Yii::$app->request->post() ) ) {
-      Yii::$app->response->format = Response::FORMAT_JSON;
-      return ActiveForm::validate( $model );
+        return $this->render('view', [
+            'model' => $this->findModel($id)
+        ]);
     }
-    if ( $model->load( Yii::$app->request->post() ) && $model->save() ) {
-      return $this->redirectSuccess( [
-          'view',
-          'id' => $model->id
-      ], "修改成功" );
-    }
-    return $this->render( 'update', [
-        'model' => $model
-    ] );
 
-  }
+    /**
+     * 创建一个新的 FriendLink 模型.
+     * 如果创建成果,浏览器将会跳转的到该模型的详情视图界面.
+     *
+     * @return mixed
+     */
+    public function actionCreate()
+    {
 
-  /**
-   * 删除一条存在的 FriendLink 模型.
-   * 如果删除成果,浏览器将会跳转的到该模型的列表视图界面.
-   *
-   * @param integer $id
-   * @return mixed
-   * @throws NotFoundHttpException 如果模型没查询到
-   */
-  public function actionDelete( $id ) {
-
-    if ( is_array( $id ) ) {
-      $modelList = FriendLink::findAll( [
-          'id' => $id
-      ] );
-      if ( $modelList ) {
-        foreach ( $modelList as $model ) {
-          $model->delete();
+        $model = new FriendLink();
+        // ajax表单验证
+        if (AppHelper::isAjaxValidationRequest() && $model->load(Yii::$app->request->post())) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return ActiveForm::validate($model);
         }
-      }
-    } else {
-      $this->findModel( $id )->delete();
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirectSuccess([
+                'view',
+                'id' => $model->id
+            ], "添加成功");
+        }
+        return $this->render('create', [
+            'model' => $model
+        ]);
     }
-    return $this->redirect( [
-        'index'
-    ] );
 
-  }
+    /**
+     * 更新一条已经存在的 FriendLink 模型.
+     * 如果更新成果,浏览器将会跳转的到该模型的详情视图界面.
+     *
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException 如果模型没查询到
+     */
+    public function actionUpdate($id)
+    {
 
-  /**
-   * 根据模型的主键Id查询 FriendLink 模型.
-   * 如果模型没有找到, 404 HTTP 异常将会抛出.
-   *
-   * @param integer $id
-   * @return FriendLink the loaded model
-   * @throws NotFoundHttpException 如果模型没查询到
-   */
-  protected function findModel( $id ) {
-
-    if ( ($model = FriendLink::findOne( [
-        'id' => $id
-    ] )) !== null ) {
-      return $model;
+        $model = $this->findModel($id);
+        // ajax表单验证
+        if (AppHelper::isAjaxValidationRequest() && $model->load(Yii::$app->request->post())) {
+            Yii::$app->response->format = Response::FORMAT_JSON;
+            return ActiveForm::validate($model);
+        }
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirectSuccess([
+                'view',
+                'id' => $model->id
+            ], "修改成功");
+        }
+        return $this->render('update', [
+            'model' => $model
+        ]);
     }
-    throw new NotFoundHttpException( Yii::t( 'app', 'The requested page does not exist.' ) );
 
-  }
+    /**
+     * 删除一条存在的 FriendLink 模型.
+     * 如果删除成果,浏览器将会跳转的到该模型的列表视图界面.
+     *
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException 如果模型没查询到
+     */
+    public function actionDelete()
+    {
+        $id = Yii::$app->request->post("id");
+        if (is_array($id)) {
+            $modelList = FriendLink::findAll([
+                'id' => $id
+            ]);
+            if ($modelList) {
+                foreach ($modelList as $model) {
+                    $model->delete();
+                }
+            }
+        } else {
+            $this->findModel($id)->delete();
+        }
+        return $this->redirect([
+            'index'
+        ]);
+    }
+
+    /**
+     * 根据模型的主键Id查询 FriendLink 模型.
+     * 如果模型没有找到, 404 HTTP 异常将会抛出.
+     *
+     * @param integer $id
+     * @return FriendLink the loaded model
+     * @throws NotFoundHttpException 如果模型没查询到
+     */
+    protected function findModel($id)
+    {
+
+        if (($model = FriendLink::findOne([
+            'id' => $id
+        ])) !== null) {
+            return $model;
+        }
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
 }
