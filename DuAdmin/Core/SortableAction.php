747e700f1b5d5f73a070085efc17b4a732a50e40
dungang <dungang@126.com>
2021-11-27 15:24:38 +0800

    add new feature for sort action

--- DuAdmin/Core/SortableAction.php
+++ DuAdmin/Core/SortableAction.php
@@ -6,11 +6,15 @@ use yii\base\Action;
 
 /**
  * 排序action
+ * 
+ * 模型至少需要满足至少有两个字段 id,pid 
+ * 如果有depth，也会自动更新
  *
  * @author dungang<dungang@126.com>
  * @since 2020-12-19
  */
-class SortableAction extends Action {
+class SortableAction extends Action
+{
 
   /**
    * 中间表的模型类
@@ -26,18 +30,18 @@ class SortableAction extends Action {
 
   public $modelClass = null;
 
-  public function run() {
+  public function run()
+  {
 
-    $sorts = \Yii::$app->request->post( 'sorts' );
-    if ( $sorts ) {
-      if ( $this->viaModelClass ) {
-        $this->updateViaChildren( $sorts, 0 );
+    $sorts = \Yii::$app->request->post('sorts');
+    if ($sorts) {
+      if ($this->viaModelClass) {
+        $this->updateViaChildren($sorts, 0);
       } else {
-        $this->updateChildren( $sorts, 0 );
+        $this->updateChildren($sorts, 0);
       }
       return $sorts;
     }
-
   }
 
   /**
@@ -46,52 +50,62 @@ class SortableAction extends Action {
    * @param array $item
    * @param number $sort
    * @param number|string $parentId
+   * @param number $depth
    * @return void
    */
-  private function updateViaItem( $item, $sort, $parentId ) {
+  private function updateViaItem($item, $sort, $parentId, $depth = 1)
+  {
 
     $viaModel = null;
     // 如果原本是root，则肯定没有关联关系存在
-    if ( $item ['pid'] !== 0 ) {
-      $activeQuery = call_user_func( [
-          $this->viaModelClass,
-          'find'
-      ] );
-      $viaModel = $activeQuery->where( [
-          $this->viaChild => $item ['id'],
-          $this->viaParent => $item ['pid']
-      ] )->limit( 1 )->one();
+    if ($item['pid'] !== 0) {
+      $activeQuery = call_user_func([
+        $this->viaModelClass,
+        'find'
+      ]);
+      $viaModel = $activeQuery->where([
+        $this->viaChild => $item['id'],
+        $this->viaParent => $item['pid']
+      ])->limit(1)->one();
     }
-    if ( $viaModel ) {
+    if ($viaModel) {
       // 如果有关联关系，更新后的pid===0，则表示变为root节点，则不需要关联关系了
-      if ( $parentId === 0 ) {
+      if ($parentId === 0) {
         $viaModel->delete();
         // 只用更新本身的排序即可
-        $this->viaUpdatetItemSort( $item, $sort );
+        $this->viaUpdatetItemSort($item, $sort, $depth);
       } else {
         // 如果有，则只需要更新parent即可
-        $viaModel [$this->viaParent] = $parentId;
+        $viaModel[$this->viaParent] = $parentId;
+        //如果有深度字段，更新深度
+        if ($viaModel->hasProperty('depth')) {
+          $viaModel->depth = $depth;
+        }
         $viaModel->sort = $sort;
-        $viaModel->save( false );
+        $viaModel->save(false);
       }
     } else {
       // 没有关联关系
       // 如果更新后的pid === 0，则表示只用更新排序即可
-      if ( $parentId === 0 ) {
-        $this->viaUpdatetItemSort( $item, $sort );
+      if ($parentId === 0) {
+        $this->viaUpdatetItemSort($item, $sort, $depth);
       } else {
         // 否则，创建关联关系
-        $viaModel = \Yii::createObject( $this->viaModelClass );
-        $viaModel [$this->viaParent] = $parentId;
-        $viaModel [$this->viaChild] = $item ['id'];
+        $viaModel = \Yii::createObject($this->viaModelClass);
+        $viaModel[$this->viaParent] = $parentId;
+        $viaModel[$this->viaChild] = $item['id'];
+        //如果有深度字段，更新深度
+        if ($viaModel->hasProperty('depth')) {
+          $viaModel->depth = $depth;
+        }
         $viaModel->sort = $sort;
-        $viaModel->save( false );
+        $viaModel->save(false);
       }
     }
-    if ( isset( $item ['children'] ) && is_array( $item ['children'] ) ) {
-      $this->updateViaChildren( $item ['children'], $item ['id'] );
+    if (isset($item['children']) && is_array($item['children'])) {
+      //如果有子集合，深度加1
+      $this->updateViaChildren($item['children'], $item['id'], $depth + 1);
     }
-
   }
 
   /**
@@ -99,17 +113,22 @@ class SortableAction extends Action {
    *
    * @param array $item
    * @param integer $sort
+   * @param number $depth
    */
-  private function viaUpdatetItemSort( $item, $sort ) {
+  private function viaUpdatetItemSort($item, $sort, $depth = 1)
+  {
 
-    $model = $this->findModel( [
-        'id' => $item ['id']
-    ] );
-    if ( $model ) {
+    $model = $this->findModel([
+      'id' => $item['id']
+    ]);
+    if ($model) {
       $model->sort = $sort;
-      $model->save( false );
+      //如果有深度字段，更新深度
+      if ($model->hasProperty('depth')) {
+        $model->depth = $depth;
+      }
+      $model->save(false);
     }
-
   }
 
   /**
@@ -117,14 +136,15 @@ class SortableAction extends Action {
    *
    * @param array $children
    * @param number|string $parentId
+   * @param number $depth
    * @return void
    */
-  private function updateViaChildren( $children, $parentId ) {
+  private function updateViaChildren($children, $parentId, $depth = 1)
+  {
 
-    foreach ( $children as $sort => $item ) {
-      $this->updateViaItem( $item, $sort, $parentId );
+    foreach ($children as $sort => $item) {
+      $this->updateViaItem($item, $sort, $parentId, $depth);
     }
-
   }
 
   /**
@@ -132,14 +152,15 @@ class SortableAction extends Action {
    *
    * @param array $children
    * @param number|string $parentId
+   * @param number $depth
    * @return void
    */
-  private function updateChildren( $children, $parentId ) {
+  private function updateChildren($children, $parentId, $depth = 1)
+  {
 
-    foreach ( $children as $sort => $item ) {
-      $this->updateItem( $item, $sort, $parentId );
+    foreach ($children as $sort => $item) {
+      $this->updateItem($item, $sort, $parentId, $depth);
     }
-
   }
 
   /**
@@ -149,34 +170,39 @@ class SortableAction extends Action {
    * @param array $item
    * @param number $sort
    * @param number|string $parentId
+   * @param number $depth
    * @return void
    */
-  private function updateItem( $item, $sort, $parentId ) {
+  private function updateItem($item, $sort, $parentId, $depth = 1)
+  {
 
-    $model = $this->findModel( [
-        'id' => $item ['id']
-    ] );
-    if ( $model ) {
+    $model = $this->findModel([
+      'id' => $item['id']
+    ]);
+    if ($model) {
       // 1层深度的时候可能没有pid
-      if ( $model->hasProperty( 'pid' ) ) {
+      if ($model->hasProperty('pid')) {
         $model->pid = $parentId;
       }
+      //如果有深度字段，更新深度
+      if ($model->hasProperty('depth')) {
+        $model->depth = $depth;
+      }
       $model->sort = $sort;
-      $model->save( false );
+      $model->save(false);
     }
-    if ( isset( $item ['children'] ) && is_array( $item ['children'] ) ) {
-      $this->updateChildren( $item ['children'], $item ['id'] );
+    if (isset($item['children']) && is_array($item['children'])) {
+      //如果有子集合，深度加1
+      $this->updateChildren($item['children'], $item['id'], $depth + 1);
     }
-
   }
 
-  private function findModel( $id ) {
+  private function findModel($id)
+  {
 
-    return call_user_func( [
-        $this->modelClass,
-        'findOne'
-    ], $id );
-
+    return call_user_func([
+      $this->modelClass,
+      'findOne'
+    ], $id);
   }
 }
-
